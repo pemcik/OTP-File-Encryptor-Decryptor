c174e8472a0c899b76b99e046c22f811bcacdd42
Andrejs Birzgals <andrejs.birzgals@gmail.com>
2023-11-01 17:56:53 +0200

    Better otp

--- otp.py
+++ otp.py
@@ -21,37 +21,47 @@ def xor_bytes(data1, data2):
 
 
 def encrypt(input_filename, otp_filename, encrypted_filename):
+    print(f"Encrypting {input_filename}...")
     data = read_file(input_filename)
     otp = generate_otp(len(data))
     encrypted_data = xor_bytes(data, otp)
 
     write_file(otp_filename, otp)
     write_file(encrypted_filename, encrypted_data)
+    print(f"Encryption complete! OTP saved as {otp_filename}, encrypted file saved as {encrypted_filename}.")
 
 
 def decrypt(encrypted_filename, otp_filename, decrypted_filename):
+    print(f"Decrypting {encrypted_filename}...")
     encrypted_data = read_file(encrypted_filename)
     otp = read_file(otp_filename)
 
     decrypted_data = xor_bytes(encrypted_data, otp)
     write_file(decrypted_filename, decrypted_data)
+    print(f"Decryption complete! Decrypted file saved as {decrypted_filename}.")
 
+
+print("")
+print("🌟 Welcome to the Ultimate OTP File Encryption & Decryption Tool 🌟")
+print("🛡️ Your go-to solution for secure file cryptography!")
+print("🌐 Visit my GitHub repository for readme, other source codes for this and more comments: https://github.com/MasterAGB/OTP-File-Encryptor-Decryptor")
+print("--------------------------------------------------------------------------------------")
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        action = input("What would you like to do? (enc/dec): ")
-        input_filename = input("Input filename: ")
-        otp_filename = input("OTP filename: ")
-        output_filename = input("Output filename: ")
+        action = input("What action would you like to perform? Encrypt (enc) or Decrypt (dec): ")
+        input_filename = input("Please provide the input filename: ")
+        otp_filename = input("Name your OTP (One-Time Pad) file: ")
+        output_filename = input("Name your output file: ")
     else:
         action = sys.argv[1]
         input_filename = sys.argv[2]
         otp_filename = sys.argv[3]
         output_filename = sys.argv[4]
 
-    if action == 'enc':
+    if action.lower() == 'enc':
         encrypt(input_filename, otp_filename, output_filename)
-    elif action == 'dec':
+    elif action.lower() == 'dec':
         decrypt(input_filename, otp_filename, output_filename)
     else:
-        print("Invalid action. Please use 'enc' for encryption or 'dec' for decryption.")
+        print("🚫 Invalid action. Kindly use 'enc' for encryption or 'dec' for decryption.")
